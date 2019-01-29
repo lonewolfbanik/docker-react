77519f95b8b11d9441b6d08666072acbd388cd98
itsmeBanik <prasanjitbanik38@gmail.com>
2019-01-29 14:50:30 +0530

    initial

---
+++ build/precache-manifest.36620dff332b49c88db2c118b4f2f9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b35e432d9f1487f4ffb",
+    "url": "/static/js/main.2b35e432.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2b35e432d9f1487f4ffb",
+    "url": "/static/css/main.a949ccd0.chunk.css"
+  },
+  {
+    "revision": "555265e7f95e449562492f3776e342aa",
+    "url": "/index.html"
+  }
+];
